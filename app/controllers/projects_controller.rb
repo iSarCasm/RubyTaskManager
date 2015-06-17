03c5453b47c449c5de4d508912bea33ff450d456
sarcasm <sarcasm008@gmail.com>
2015-06-17 21:50:21 +0300

    project creating fixed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,27 +1,27 @@
 class ProjectsController < ApplicationController
-  def create
-  	@project = Project.new(	name: 	get_params[:name],
-  													date: 	Time.zone.now)
-  	if @project.save
+	def create
+		@project = current_user.projects.new(	name: 	get_params[:name],
+																				 date: 	Time.zone.now)
+		if @project.save
 			flash[:succes] = "ez pz"
-  	else 
-  		flash[:danger] = "wtf"
-  	end
-  	redirect_to root_url
-  end
+		else 
+			flash[:danger] = "wtf"
+		end
+		redirect_to root_url
+	end
 
-  def update
-    Project.find(params[:id]).update(name: get_params[:name])
-    redirect_to root_url
-  end
+	def update
+		Project.find(params[:id]).update(name: get_params[:name])
+		redirect_to root_url
+	end
 
-  def destroy
-    Project.find(params[:id]).destroy
-    redirect_to root_url
-  end
+	def destroy
+		Project.find(params[:id]).destroy
+		redirect_to root_url
+	end
 
-  private 
-  	def get_params
-  		params.require(:project).permit(:name)
-  	end
+	private 
+		def get_params
+			params.require(:project).permit(:name)
+		end
 end
