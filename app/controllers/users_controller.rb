77d67fc96eca483006473dcf04b0c4bc5b83f1be
sarcasm <sarcasm008@gmail.com>
2015-06-26 19:58:27 +0300

    lol

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,19 +27,23 @@ class UsersController < ApplicationController
     @header = Array.new
 
     @query[0] = 'Task.select(:done, :deadline).order(name: :asc).distinct'
-    @sql[0] = Task.select("projects.name AS pName","tasks.*").joins(:project).where("projects.name LIKE ?",'N%')
+    @sql[0] = Project.joins("LEFT OUTER JOIN tasks ON 'projects'.'id'='tasks'.'project_id'").group(:project_id).select("projects.*, COUNT(tasks.project_id) as TaskCount")
+              .where("projects.name LIKE ?","%_a_%")
     @header[0] = ["ID","Done?","Deadline"]
 
     @query[1] = 'Task.joins(:project).group(:project_id).select("projects.name, COUNT(*) as TaskCount").order("TaskCount DESC")'
-    @sql[1] = Task.select("projects.name AS pName","tasks.*").joins(:project).where("projects.name LIKE ?",'N%')
+    @sql[1] = Project.joins("LEFT OUTER JOIN tasks ON 'projects'.'id'='tasks'.'project_id'").group(:project_id).select("projects.*, COUNT(tasks.project_id) as TaskCount")
+              .where("projects.name LIKE ?","%_a_%")
     @header[1] = ["ID", "Project name","Task Count"]
 
     @query[2] = 'Task.joins(:project).group(:project_id).select("projects.name, COUNT(*) as TaskCount").order("projects.name ASC")'
-    @sql[2] = Task.select("projects.name AS pName","tasks.*").joins(:project).where("projects.name LIKE ?",'N%')
+    @sql[2] = Project.joins("LEFT OUTER JOIN tasks ON 'projects'.'id'='tasks'.'project_id'").group(:project_id).select("projects.*, COUNT(tasks.project_id) as TaskCount")
+              .where("projects.name LIKE ?","%_a_%")
     @header[2] = ["ID", "Project name","Task Count"]
 
     @query[3] = ' Task.select("projects.name AS pName","tasks.*").joins(:project).where("projects.name LIKE ?","N%"")'
-    @sql[3] = Task.select("projects.name AS pName","tasks").joins(:project).where("projects.name LIKE ?",'N%')
+    @sql[3] = Project.joins("LEFT OUTER JOIN tasks ON 'projects'.'id'='tasks'.'project_id'").group(:project_id).select("projects.*, COUNT(tasks.project_id) as TaskCount")
+              .where("projects.name LIKE ?","%_a_%")
     @header[3] = ["ID", "Name","priority","done?","deadline","project_id","created_at","updated_at","Project name"]
 
     @query[4] = 'Project.joins("LEFT OUTER JOIN tasks ON projects.id = tasks.project_id").group(:project_id).select("projects.*, COUNT(tasks.project_id) as TaskCount")
